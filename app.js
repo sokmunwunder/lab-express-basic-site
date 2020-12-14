ffd03ef5fe32c3834ae59fe5ed83404966788490
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-12-14 17:21:00 +0100

    1st basic hbs working site

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 const app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 app.use(express.static('public'));
+
+app.get('/home', (request, response) => {
+  response.render(__dirname + '/views/home.hbs');
+});
+
+app.get('/works', (request, response) => {
+  response.render(__dirname + '/views/works.hbs');
+});
+
+app.get('/about', (request, response) => {
+  response.render(__dirname + '/views/about.hbs');
+});
+
+app.get('/photos', (request, response) => {
+  response.render(__dirname + '/views/photos.hbs');
+});
+
+app.listen(3000);
 
 /*app.get('/home', function (request, response) {
   response.send('Testing');
@@ -19,7 +45,7 @@ app.get('/gallery', function (request, response) {
   response.send('Testing 3');
 });*/
 
-app.get('/home', function (request, response) {
+/*app.get('/home', function (request, response) {
   response.sendFile(__dirname + '/views/home.html');
 });
 
@@ -33,6 +59,4 @@ app.get('/works', function (request, response) {
 
 app.get('/gallery', function (request, response) {
   response.sendFile(__dirname + '/views/gallery.html');
-});
-
-app.listen(3000);
+});*/
